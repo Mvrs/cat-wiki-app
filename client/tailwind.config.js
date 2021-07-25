48f6d563fae2d4d8e1fbedb325c4e5257cc7bca6
Marlon Johnson <johnsonmarlon18@gmail.com>
2021-07-25 15:26:49 -0700

    setup tailwindcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  purge: { enabled: false, content: ["src/**/*.tsx"] },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {},
+      spacing: {
+        68: "17rem",
+        29: "7.25rem",
+        37: "9.25rem",
+        88: "22rem",
+      },
+      textColor: {
+        gray: { 250: "#E0E0E0", 550: "#544439", 750: "#291507" },
+      },
+      backgroundColor: {
+        gray: { 250: "#E0E0E0", 550: "#544439", 750: "#291507" },
+        yellow: { 550: "#DEC68B" },
+      },
+      fontFamily: {
+        mont: ["Montserrat"],
+      },
+      keyframes: {
+        "to-left": {
+          "0%": { transform: "translate(30px)", opacity: 0 },
+          "100%": { transform: "translate(0)", opacity: 100 },
+        },
+      },
+      animation: {
+        "to-left": "to-left 0.3s ease-out forwards",
+      },
+      gridTemplateColumns: {
+        "2-auto": "auto auto",
+        "2-info": "190px auto",
+      },
+      gridTemplateRows: {
+        "2-auto": "auto auto",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      animation: ["group-hover"],
+      backgroundColor: ["active"],
+    },
+  },
+  plugins: [require("tailwind-scrollbar")],
+};
