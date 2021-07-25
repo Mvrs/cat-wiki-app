e1d876eec3912fbfd7eac063ce2d95a1453edce7
Marlon Johnson <johnsonmarlon18@gmail.com>
2021-07-25 02:06:56 -0700

    add model for count of breeds

---
+++ server/src/app.ts
@@ -0,0 +1,4 @@
+import { mongo_uri } from "./config/index.config";
+import cors from "cors";
+import express, { Application } from "express";
+import mongoose from "mongoose";
--- server/src/config/index.config.ts
+++ server/src/config/index.config.ts
@@ -3,4 +3,4 @@ config();
 
 export const port = Number(process.env.PORT);
 export const catAPIKey = process.env.CAT_API_KEY;
-export const mong_uri = process.env.MONGO_URI as string;
+export const mongo_uri = process.env.MONGO_URI as string;
